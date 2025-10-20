2bba6b6c05f52640e887175f7ec23bc0ee2515cd
Dana Badarneh <135717996+danabadarneh@users.noreply.github.com>
2025-10-20 21:43:04 +0300

    Refactor test_highlight_github_star function

--- first.py
+++ first.py
@@ -8,14 +8,14 @@ def test_highlight_search(page:Page):
 def test_highlight_getstarted(page :Page):
     page.goto("https://playwright.dev/python/")
     page.get_by_role("link",name="Get started").click()
+    
 def test_highlight_github_star(page: Page):
     page.goto("https://playwright.dev/python/")
- 
     star_button = page.locator('[aria-label="Star microsoft/playwright-python on GitHub"]')
     star_button.highlight()
     star_button.click()  
-   
     page.wait_for_timeout(5000)
+    
 def test_highlight_star2(page: Page ):
     page.goto("https://playwright.dev/python/")
     page.locator('[aria-label="78k+ stargazers on GitHub"]').highlight()

c2af46d356cba56df36e127fbad7408c3b4bee0a
Dana Badarneh <danabadarneh@192.168.1.7>
2025-10-20 21:36:52 +0300

    Initial commit

---
+++ first.py
@@ -0,0 +1,22 @@
+from playwright.sync_api import Page, expect
+def test_highlight_search(page:Page):
+    page.goto("https://playwright.dev/python/")
+    search_icon = page.locator('.DocSearch-Search-Icon')
+    search_icon.highlight();
+    search_icon.click();
+
+def test_highlight_getstarted(page :Page):
+    page.goto("https://playwright.dev/python/")
+    page.get_by_role("link",name="Get started").click()
+def test_highlight_github_star(page: Page):
+    page.goto("https://playwright.dev/python/")
+    # تسليط الضوء على زر Star
+    star_button = page.locator('[aria-label="Star microsoft/playwright-python on GitHub"]')
+    star_button.highlight()
+    star_button.click()  
+    # انتظر 5 ثواني لتشوفي التغيير
+    page.wait_for_timeout(5000)
+def test_highlight_star2(page: Page ):
+    page.goto("https://playwright.dev/python/")
+    page.locator('[aria-label="78k+ stargazers on GitHub"]').highlight()
+    
